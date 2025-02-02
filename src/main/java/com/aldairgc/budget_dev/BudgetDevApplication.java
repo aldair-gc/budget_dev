b8f49f6a4dc87675011c081fe5a2e1c6d7dba44b
aldairgc <dev@aldairgc.com>
2025-02-02 13:59:34 -0300

    Initial commit

---
+++ src/main/java/com/aldairgc/budget_dev/BudgetDevApplication.java
@@ -0,0 +1,13 @@
+package com.aldairgc.budget_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldairgc/budget_dev/BudgetDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldairgc.budget_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
